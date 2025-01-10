f749de2d92de5e45965de9a6ab756adf42a0a9f5
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2025-01-10 13:16:00 +0545

    completed

---
+++ code.py
@@ -0,0 +1,11 @@
+# Define the numbers
+num1 = 5
+num2 = 3
+num3 = 4
+num4 = 1
+
+# Compute the formula
+result = ((num1 * num2) * (num3 - num4)) ** 2
+
+# Print the result
+print("The result of the formula is:", result)
